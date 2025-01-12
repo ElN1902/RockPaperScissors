aad9004167fc8f0a527453fcb5885a9a615f4e9f
El_N <enagaeva19@gmail.com>
2025-01-13 00:52:10 +0300

    Refactor the code Change playRound function for better readability. Add structured output to console for better readability. Add capitalize function for visually appealing presentation of the output.

--- game.js
+++ game.js
@@ -2,7 +2,7 @@
 // GET computer's choice
 function getComputerChoice() {
     let randomNumber = Math.random() * 100;
-    if (randomNumber >= 0 && randomNumber < 34) {
+    if (randomNumber < 34) {
      return 'rock';
     } 
     else if (randomNumber >=34 && randomNumber < 77) {
@@ -16,97 +16,81 @@ function getComputerChoice() {
  
  // GET human choice
  function getHumanChoice() {
-     var move = prompt('What is your move: ');
-     move = move.toLowerCase();
-     while (move != 'rock' && move != 'paper' && move != 'scissors') {
-         move = prompt('What is your move: ');
+     let move = prompt('What is your move: Rock, Paper or Scissors? ').toLowerCase();
+
+     // VALIDATE the user's input
+     while (move !== 'rock' && move !== 'paper' && move !== 'scissors') {
+         move = prompt('Invalid input. Please enter Rock, Paper or Scissors: ').toLowerCase();
            }
      return move;
  }
  
  
- // PLAY one round
+ // DEFINE the winner
  function playRound(humanChoice, computerChoice) {
-     // COMPARE the computer and human choices
-     let winner;
-     if (humanChoice === 'rock') {
-         if (computerChoice === 'paper') {
-             console.log('You lose! Paper beats Rock.');
-             winner = 'computer';
-             return winner;
-         }
-         else if (computerChoice === 'scissors') {
-             console.log('You won! Rock beats Scissors');
-             winner = 'human'
-             return winner;
-         }
-         else {
-             console.log('Draw!')
-         }
+     if (humanChoice === computerChoice) {
+         console.log("It's a tie!");
+         return -1;
      }
-     else if (humanChoice === 'paper') {
-         if (computerChoice === 'rock') {
-             console.log('You won! Paper beats Rock.');
-             winner = 'human';
-             return winner;
-         }
-         else if (computerChoice === "scissors") {
-             console.log('You lose! Scissors beat Paper.');
-             winner = 'computer';
-             return winner;
-         }
-         else {
-             console.log('Draw!');
-         }
-     }
+     else if ((humanChoice === 'paper' && computerChoice === 'rock') ||
+                (humanChoice === 'rock' && computerChoice === 'scissors') ||
+                (humanChoice === 'scissors' && computerChoice === 'paper')) 
+                {
+                    console.log(`You won! ${cap(humanChoice)} beats ${cap(computerChoice)}`);
+                    return 1;
+                }
      else {
-         if (computerChoice === 'rock') {
-             console.log('You lose! Rock beats scissors.');
-             winner = 'computer';
-             return winner;
-         }
-         else if (computerChoice === 'paper') {
-             console.log('You won! Scissors beat paper.');
-             winner = 'human';
-             return winner;
-         }
-         else {
-             console.log('Draw!');
-         }
-     }
-     }
- 
- 
- // REPEAT 5 times
- function playGame() {
-     var humanScore = 0;
-     var computerScore = 0;
- 
-     for (let i = 0; i < 5; i++) {
-         var humanChoice = getHumanChoice();
-         console.log(humanChoice);
-         var computerChoice = getComputerChoice();
-         console.log(computerChoice);
-         let score = playRound(humanChoice, computerChoice);
-         if (score === 'computer') {
-             computerScore += 1;
-         }
-         else if (score === 'human') {
-             humanScore += 1;
-         }
-     }
- 
-     if (computerScore > humanScore) {
-         console.log('You lose the game!');
-     }
-     else if(computerScore < humanScore) {
-         console.log('Congratulations! You won the game');
-     }
-     else {
-         console.log('Draw!');
+         console.log(`You lost. ${cap(computerChoice)} beats ${cap(humanChoice)}`);
+         return 0;
      }
  }
  
+ // INITIATE the game
+ function playGame() {
+    // INITIALIZE human and computer scores at the beginning of the game
+     var humanScore = 0;
+     var computerScore = 0;
+    // REPEAT round 5 times
+     for (let i = 1; i <= 5; i++) {
+         let humanChoice = getHumanChoice();
+
+         // STRUCTURE the output
+         console.group(`Round ${i}`);
+         console.log(`Your move: ${humanChoice}`);
+         let computerChoice = getComputerChoice();
+         console.log(`Computer move: ${computerChoice}`);
+         let score = playRound(humanChoice, computerChoice);
+        // INCREMENT the human or computer score depending on the winner of the round
+         if (score === 0) {
+             computerScore += 1;
+         }
+         else if (score === 1) {
+             humanScore += 1;
+         }
+         console.groupEnd(`Round ${i}`);
+     }
+
+     console.group('The game is over.');
+
+     // FIND the winner
+     if (computerScore > humanScore) {
+         console.log('You lose the game! Better luck next time!');
+     }
+     else if(computerScore < humanScore) {
+         console.log('Congratulations! You won the game.');
+     }
+     else {
+         console.log('It\'s a tie!');
+     }
+     console.groupEnd('The game is over.')
+ }
+
+ // CAPITALIZE the input
+ function cap(string) {
+    let capString = string.charAt(0).toUpperCase() + string.slice(1);
+    return capString;
+ }
+
  playGame();
  
 
